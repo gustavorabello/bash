0085b66f1de3671bbdc6a97459e6ff30a26eb06a
Gustavo ANJOS <gustavo.rabello@gmail.com>
2008-08-05 08:34:28 +0000

    adicionando comentarios

--- replace.sh
+++ replace.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-for file in simC-???.vtk; do
+# this routine makes a loop over the files (simC-*.vtk) modifying the
+# exponential values lower then 41, it means: values < 10-e41 changing
+# to the value 10e-40. Also, this rotine makes a backup copy in each
+# file associating the .old extension to the original name.
+for file in simC-*.vtk; do
  for i in $(gseq 41 120); do
   var+='-e s/e-'${i}'/e-40/g ';
  done
- sed $var $file > ./new/$;
- echo $file modificado e transferido para ./new/
+ mv $file $file.old
+ sed $var $file.old > $file;
+ echo $file modificado e criado backup
 done
 
 
