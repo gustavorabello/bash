ecc52ea2ffb568eab383ecf367fc19412d375eba
Gustavo ANJOS <gustavo.rabello@gmail.com>
2008-09-30 15:28:36 +0000

    correcoes...

--- projectSwitch.sh
+++ projectSwitch.sh
@@ -7,115 +7,115 @@ SVN=http://svn.rabello.org/gustavo
 
 DIR=~/projects/cpp/ns2d
 AUX=$DIR/trunk
-cd ~/$DIR
+cd $DIR
 svn switch --relocate $SVNOLD1/$AUX $SVN/$AUX
 svn switch --relocate $SVNOLD2/$AUX $SVN/$AUX
 
 DIR=~/projects/cpp/ns2dTransport
 AUX=$DIR/trunk
-cd ~/$DIR
+cd $DIR
 svn switch --relocate $SVNOLD1/$AUX $SVN/$AUX
 svn switch --relocate $SVNOLD2/$AUX $SVN/$AUX
 
 DIR=~/projects/cpp/ns3d
 AUX=$DIR/trunk
-cd ~/$DIR
+cd $DIR
 svn switch --relocate $SVNOLD1/$AUX $SVN/$AUX
 svn switch --relocate $SVNOLD2/$AUX $SVN/$AUX
 
 DIR=~/projects/cpp/ns3dTransport
 AUX=$DIR/trunk
-cd ~/$DIR
+cd $DIR
 svn switch --relocate $SVNOLD1/$AUX $SVN/$AUX
 svn switch --relocate $SVNOLD2/$AUX $SVN/$AUX
 
 DIR=~/projects/cpp/meshGen
 AUX=$DIR/trunk
-cd ~/$DIR
+cd $DIR
 svn switch --relocate $SVNOLD1/$AUX $SVN/$AUX
 svn switch --relocate $SVNOLD2/$AUX $SVN/$AUX
 
 DIR=~/projects/python/ns2d
 AUX=$DIR/trunk
-cd ~/$DIR
+cd $DIR
 svn switch --relocate $SVNOLD1/$AUX $SVN/$AUX
 svn switch --relocate $SVNOLD2/$AUX $SVN/$AUX
 
 DIR=~/projects/python/ns2dTransport
 AUX=$DIR/trunk
-cd ~/$DIR
+cd $DIR
 svn switch --relocate $SVNOLD1/$AUX $SVN/$AUX
 svn switch --relocate $SVNOLD2/$AUX $SVN/$AUX
 
 DIR=~/projects/python/ns3d
 AUX=$DIR/trunk
-cd ~/$DIR
+cd $DIR
 svn switch --relocate $SVNOLD1/$AUX $SVN/$AUX
 svn switch --relocate $SVNOLD2/$AUX $SVN/$AUX
 
 DIR=~/projects/python/ns3dTransport
 AUX=$DIR/trunk
-cd ~/$DIR
+cd $DIR
 svn switch --relocate $SVNOLD1/$AUX $SVN/$AUX
 svn switch --relocate $SVNOLD2/$AUX $SVN/$AUX
 
 DIR=~/projects/python/misc
 AUX=$DIR/trunk
-cd ~/$DIR
+cd $DIR
 svn switch --relocate $SVNOLD1/$AUX $SVN/$AUX
 svn switch --relocate $SVNOLD2/$AUX $SVN/$AUX
 
 DIR=~/projects/python/heat
 AUX=$DIR/trunk
-cd ~/$DIR
+cd $DIR
 svn switch --relocate $SVNOLD1/$AUX $SVN/$AUX
 svn switch --relocate $SVNOLD2/$AUX $SVN/$AUX
 
 DIR=~/projects/matlab/ns2d
 AUX=$DIR/trunk
-cd ~/$DIR
+cd $DIR
 svn switch --relocate $SVNOLD1/$AUX $SVN/$AUX
 svn switch --relocate $SVNOLD2/$AUX $SVN/$AUX
 
 DIR=~/projects/matlab/ns2dTransport
 AUX=$DIR/trunk
-cd ~/$DIR
+cd $DIR
 svn switch --relocate $SVNOLD1/$AUX $SVN/$AUX
 svn switch --relocate $SVNOLD2/$AUX $SVN/$AUX
 
 DIR=~/projects/matlab/ns2d/
 AUX=$DIR/trunk
-cd ~/$DIR
+cd $DIR
 svn switch --relocate $SVNOLD1/$AUX $SVN/$AUX
 svn switch --relocate $SVNOLD2/$AUX $SVN/$AUX
 
 DIR=~/projects/matlab/ns2dTransport
 AUX=$DIR/trunk
-cd ~/$DIR
+cd $DIR
 svn switch --relocate $SVNOLD1/$AUX $SVN/$AUX
 svn switch --relocate $SVNOLD2/$AUX $SVN/$AUX
 
 DIR=~/projects/matlab/ns3d
 AUX=$DIR/trunk
-cd ~/$DIR
+cd $DIR
 svn switch --relocate $SVNOLD1/$AUX $SVN/$AUX
 svn switch --relocate $SVNOLD2/$AUX $SVN/$AUX
 
 DIR=~/projects/matlab/ns3dTransport
 AUX=$DIR/trunk
-cd ~/$DIR
+cd $DIR
 svn switch --relocate $SVNOLD1/$AUX $SVN/$AUX
 svn switch --relocate $SVNOLD2/$AUX $SVN/$AUX
 
 DIR=~/projects/bash
 AUX=$DIR/trunk
-cd ~/$DIR
+cd $DIR
 svn switch --relocate $SVNOLD1/$AUX $SVN/$AUX
 svn switch --relocate $SVNOLD2/$AUX $SVN/$AUX
 
 DIR=~/dotfiles
 AUX=$DIR/trunk
-cd ~/$DIR
+cd $DIR
 svn switch --relocate $SVNOLD1/$AUX $SVN/$AUX
 svn switch --relocate $SVNOLD2/$AUX $SVN/$AUX
 
