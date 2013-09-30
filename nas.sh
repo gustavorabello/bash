031fd98189dcc5517132cd0f8f1bda68cbe95775
Gustavo ANJOS <gustavo.rabello@gmail.com>
2013-09-29 23:42:15 -0300

    updated nas.sh.

--- nas.sh
+++ nas.sh
@@ -3,19 +3,30 @@
 echo ""
 echo "...updating Pictures"
 echo ""
-rsync -avz --exclude 'iP*' $HOME/Pictures/ /Volumes/gustavo/Pictures/
+rsync -avz \
+      --exclude 'iP*' \
+	  --exclude '.Apple*' \
+	  $HOME/Pictures/ /Volumes/gustavo/Pictures/
 echo ""
 echo "...updating Documents"
 echo ""
-rsync -avz --exclude '.temp*' $HOME/Documents/ /Volumes/gustavo/Documents/
+rsync -avz \
+      --exclude '.temp*' \
+	  --exclude '.Apple*' \
+	  $HOME/Documents/ /Volumes/gustavo/Documents/
 echo ""
-echo "...updating Musics"
+echo "...updating Musics at Media folder"
 echo ""
-rsync -avz --exclude 'iTunes*' $HOME/Music/ /Volumes/gustavo/Music/
+rsync -avz \
+      --exclude 'iTunes*' \
+	  --exclude '.Apple*' \
+	  $HOME/Music/ /Volumes/media/Music/
 echo ""
-echo "...updating Movies"
+echo "...updating Movies at Media folder"
 echo ""
-rsync -avz $HOME/Movies/ /Volumes/media/Movies/
+rsync -avz \
+      --exclude '.Apple*' \
+	  $HOME/Movies/ /Volumes/media/Movies/
 
 # $Id: $
 
