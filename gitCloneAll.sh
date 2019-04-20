7019688e91d0ca26de4dfb4a507184f8752464da
Gustavo ANJOS <gustavo.rabello@gmail.com>
2019-04-20 10:44:41 -0300

    Updated gitCloneAll

--- gitCloneAll.sh
+++ gitCloneAll.sh
@@ -7,82 +7,112 @@ mkdir $PROJECT
 cd $PROJECT
 
 # c++
-git clone $GIT/cpp-femSIM2d cpp/femSIM2d
-git clone $GIT/cpp-femSIM3d cpp/femSIM3d
-git clone $GIT/cpp-lib cpp/lib
-git clone $GIT/cpp-maxwell2d cpp/maxwell2d
+DIR=$HOME/projects/cpp
+git clone $GIT/cpp-femSIM2d             $DIR/femSIM2d
+git clone $GIT/cpp-femSIM3d             $DIR/femSIM3d
+git clone $GIT/cpp-lib                  $DIR/lib
+git clone $GIT/cpp-maxwell2d            $DIR/maxwell2d
 
 # matlab
-git clone $GIT/matlab-ns2d matlab/ns2d
-git clone $GIT/matlab-ns2dBubble matlab/ns2dBubble
-git clone $GIT/matlab-ns2dTransport matlab/ns2dTransport
-git clone $GIT/matlab-ns3d matlab/ns3d
-git clone $GIT/matlab-ns3dTransport matlab/ns3dTransport
-git clone $GIT/matlab-curvature matlab/curvature
+DIR=$HOME/projects/matlab
+git clone $GIT/matlab-ns2d              $DIR/ns2d
+git clone $GIT/matlab-ns2dBubble        $DIR/ns2dBubble
+git clone $GIT/matlab-ns2dTransport     $DIR/ns2dTransport
+git clone $GIT/matlab-ns3d              $DIR/ns3d
+git clone $GIT/matlab-ns3dTransport     $DIR/ns3dTransport
+git clone $GIT/matlab-curvature         $DIR/curvature
 
 # python 
-git clone $GIT/python-mech-course python/mech-course
-git clone $GIT/python-misc python/misc
-git clone $GIT/python-femSIM2d python/femSIM2d
-git clone $GIT/python-femSIM3d python/femSIM3d
-git clone $GIT/python-personal-site python/site-personal
-git clone $GIT/gustavorabello.github.io python/site-personal/gustavorabello.github.io
-git clone $GIT/python-pyAnalytics python/pyAnalytics
-git clone $GIT/python-htmlSIM2d python/htmlSIM2d
-git clone $GIT/python-maxwell2d python/maxwell2d
-git clone $GIT/python-pyFEM2d python/pyFEM2d
-git clone $GIT/python-pyStreamVort2d python/pyStreamVort2d
-git clone $GIT/python-transcal python/transcal
-git clone $GIT/python-pyVonKarman python/pyVonKarman
-git clone $GIT/python-poisson python/poisson
-git clone $GIT/python-heat python/heat
+DIR=$HOME/projects/python
+git clone $GIT/python-mech-course       $DIR/mech-course
+git clone $GIT/python-misc              $DIR/misc
+git clone $GIT/python-femSIM2d          $DIR/femSIM2d
+git clone $GIT/python-femSIM3d          $DIR/femSIM3d
+git clone $GIT/python-personal-site     $DIR/site-personal
+git clone $GIT/python-pyAnalytics       $DIR/pyAnalytics
+git clone $GIT/python-htmlSIM2d         $DIR/htmlSIM2d
+git clone $GIT/python-maxwell2d         $DIR/maxwell2d
+git clone $GIT/python-pyFEM2d           $DIR/pyFEM2d
+git clone $GIT/python-pyStreamVort2d    $DIR/pyStreamVort2d
+git clone $GIT/python-transcal          $DIR/transcal
+git clone $GIT/python-pyVonKarman       $DIR/pyVonKarman
+git clone $GIT/python-poisson           $DIR/poisson
+git clone $GIT/python-heat              $DIR/heat
+git clone $GIT/gustavorabello.github.io $DIR/site-personal/gustavorabello.github.io
+
+# bash
+DIR=$HOME/projects/bash
+git clone $GIT/bash                     $DIR
 
 # misc
-git clone $GIT/misc-latex misc/latex
-git clone $GIT/dotfiles misc/dotfiles
+DIR=$HOME/projects/misc
+git clone $GIT/misc-latex               $DIR/latex
+git clone $GIT/dotfiles                 $DIR/dotfiles
+git clone $GIT/paraview                 $DIR/paraview
+
 
 # db
-git clone $GIT/gmsh-2d db/gmsh/2d
-git clone $GIT/gmsh-3d db/gmsh/3d
-git clone $GIT/baseState db/baseState
+DIR=$HOME/projects/db
+git clone $GIT/gmsh-2d                   $DIR/gmsh/2d
+git clone $GIT/gmsh-3d                   $DIR/gmsh/3d
+git clone $GIT/baseState                 $DIR/baseState
 
 # articles
-git clone $GIT/articles-2012-jcp articles/2012/jcp
-git clone $GIT/articles-2012-jes articles/2012/jes
-git clone $GIT/articles-2012-phd articles/2012/phd
-git clone $GIT/articles-2012-eci articles/2012/eci
-git clone $GIT/articles-2013-hte articles/2013/hte
-git clone $GIT/articles-2013-johnBook articles/2013/johnBook
-git clone $GIT/articles-2013-jbsmse articles/2013/bmse
-git clone $GIT/articles-2015-opteng articles/2015/opteng
-git clone $GIT/articles-2015-interpack articles/2015/interpack
-git clone $GIT/articles-2017-johnBook-applications articles/2017/johnBook
-git clone $GIT/articles-2017-exHFT-9 articles/2017/exHFT-9
-git clone $GIT/articles-2018-ijnme articles/2018/ijnme
-git clone $GIT/articles-2018-bmse articles/2018/bmse
-git clone $GIT/articles-2019-icmf articles/2019/icmf
-git clone $GIT/articles-2019-cmame articles/2019/cmame
+DIR=$HOME/projects/articles
+git clone $GIT/articles-2003-cesam                 $DIR/2003/cesam
+git clone $GIT/articles-2003-semic                 $DIR/2003/semic
+git clone $GIT/articles-2004-encit                 $DIR/2004/encit
+git clone $GIT/articles-2007-msc-dissertation      $DIR/2007/msc/dissertation
+git clone $GIT/articles-2007-msc-presentation      $DIR/2007/msc/presentation
+git clone $GIT/articles-2007-msc/ns3d              $DIR/2007/msc/ns3d
+git clone $GIT/articles-2007-capes                 $DIR/2007/capes
+git clone $GIT/articles-2007-cnmac                 $DIR/2007/cnmac
+git clone $GIT/articles-2007-emc                   $DIR/2007/emc
+git clone $GIT/articles-2007-emcLivro              $DIR/2007/emcLivro
+git clone $GIT/articles-2007-iciam-disk            $DIR/2007/iciam/disk
+git clone $GIT/articles-2007-iciam-reservoirs      $DIR/2007/iciam/reservoirs
+git clone $GIT/articles-2011-epflReport            $DIR/2011/epflReport
+git clone $GIT/articles-2011-etpfgm                $DIR/2011/etpfgm
+git clone $GIT/articles-2011-jes                   $DIR/2011/jes
+git clone $GIT/articles-2011-nano-tera             $DIR/2011/nano-tera
+git clone $GIT/articles-2012-jcp                   $DIR/2012/jcp
+git clone $GIT/articles-2012-jes                   $DIR/2012/jes
+git clone $GIT/articles-2012-phd                   $DIR/2012/phd
+git clone $GIT/articles-2012-eci                   $DIR/2012/eci
+git clone $GIT/articles-2013-hte                   $DIR/2013/hte
+git clone $GIT/articles-2013-johnBook              $DIR/2013/johnBook
+git clone $GIT/articles-2013-jbsmse                $DIR/2013/bmse
+git clone $GIT/articles-2015-opteng                $DIR/2015/opteng
+git clone $GIT/articles-2015-interpack             $DIR/2015/interpack
+git clone $GIT/articles-2017-johnBook-applications $DIR/2017/johnBook
+git clone $GIT/articles-2017-exHFT-9               $DIR/2017/exHFT-9
+git clone $GIT/articles-2018-ijnme                 $DIR/2018/ijnme
+git clone $GIT/articles-2018-bmse                  $DIR/2018/bmse
+git clone $GIT/articles-2019-icmf                  $DIR/2019/icmf
+git clone $GIT/articles-2019-cmame                 $DIR/2019/cmame
 
 # supports
-git clone $GIT/supports-2014-faperj-inst supports/2014/faperj/inst
-git clone $GIT/supports-2015-cnpq-produtividade supports/2015/cnpq/produtividade
-git clone $GIT/supports-2015-faperj-adt1 supports/2015/faperj/adt1
-git clone $GIT/supports-2015-faperj-apq1 supports/2015/faperj/apq1
-git clone $GIT/supports-2015-faperj-apq5 supports/2015/faperj/apq5
-git clone $GIT/supports-2015-uerj-catedras supports/2015/uerj/catedras
-git clone $GIT/supports-2016-faperj-jcne supports/2016/faperj/jcne
-git clone $GIT/supports-2016-faperj-emergentes supports/2016/faperj/emergentes
-git clone $GIT/supports-2016-cnpq-produtividade supports/2016/cnpq/produtividade
-git clone $GIT/supports-2017-cnpq-produtividade supports/2017/cnpq/produtividade
-git clone $GIT/supports-2018-capes-paep supports/2018/capes/paep
-git clone $GIT/supports-2018-cnpq-universal supports/2018/cnpq/universal
-git clone $GIT/supports-2018-cnpq-produtividade supports/2018/cnpq/produtividade
+DIR=$HOME/projects/supports
+git clone $GIT/supports-2014-faperj-inst        $DIR/2014/faperj/inst
+git clone $GIT/supports-2015-cnpq-produtividade $DIR/2015/cnpq/produtividade
+git clone $GIT/supports-2015-faperj-adt1        $DIR/2015/faperj/adt1
+git clone $GIT/supports-2015-faperj-apq1        $DIR/2015/faperj/apq1
+git clone $GIT/supports-2015-faperj-apq5        $DIR/2015/faperj/apq5
+git clone $GIT/supports-2015-uerj-catedras      $DIR/2015/uerj/catedras
+git clone $GIT/supports-2016-faperj-jcne        $DIR/2016/faperj/jcne
+git clone $GIT/supports-2016-faperj-emergentes  $DIR/2016/faperj/emergentes
+git clone $GIT/supports-2016-cnpq-produtividade $DIR/2016/cnpq/produtividade
+git clone $GIT/supports-2017-cnpq-produtividade $DIR/2017/cnpq/produtividade
+git clone $GIT/supports-2018-capes-paep         $DIR/2018/capes/paep
+git clone $GIT/supports-2018-cnpq-universal     $DIR/2018/cnpq/universal
+git clone $GIT/supports-2018-cnpq-produtividade $DIR/2018/cnpq/produtividade
 
-# latex
-git clone $GIT/latex-mech-course notes/mech-course
-git clone $GIT/latex-transCal notes/transCal
-git clone $GIT/latex-elementosFinitos notes/elementosFinitos
-git clone $GIT/latex-memorial-ufrj notes/memorial-ufrj
+# notes-latex
+DIR=$HOME/projects/notes
+git clone $GIT/latex-mech-course                $DIR/mech-course
+git clone $GIT/latex-transCal                   $DIR/transCal
+git clone $GIT/latex-elementosFinitos           $DIR/elementosFinitos
+git clone $GIT/latex-memorial-ufrj              $DIR/memorial-ufrj
 
 # $Id: $
 
